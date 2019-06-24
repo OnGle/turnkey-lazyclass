7a4dec50c825a46537fedaa27700164173ad1fdc
Stefan Davis <stefan@turnkeylinux.org>
2019-06-24 03:34:41 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='TurnkeyLazyClass',
+    version='1.0',
+    description='Python library for lazy class initialization',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-lazyclass',
+    packages=['lazyclass'])
